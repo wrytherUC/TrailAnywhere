b85f6a803c40e424eba8e90dec90dc0a79dbbf55
wryther <rytherwy@mail.uc.edu>
2023-09-19 22:03:53 -0400

    Added vars, actions to for 1st unit test

--- src/test/java/com/trailanywhere/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/trailanywhere/enterprise/EnterpriseApplicationTests.java
@@ -1,10 +1,18 @@
 package com.trailanywhere.enterprise;
 
+import com.trailanywhere.enterprise.dto.Trail;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EnterpriseApplicationTests {
+
+    @Autowired
+    private ITrailService trailService;
+    private Trail trail;
 
     @Test
     void contextLoads() {
@@ -12,19 +20,21 @@ class EnterpriseApplicationTests {
 
     @Test
     void fetchTrailByName_returnTrailID1ForTrailForrestPark() {
-        givenTrailDataIsAvailableWithTrailForrestPark();
+        givenTrailDataIsAvailableWithTrailNameForrestPark();
         whenSearchTrailWithNameForrestPark();
         thenReturnTrailID1TrailForForrestPark();
     }
 
-    private void givenTrailDataIsAvailableWithTrailForrestPark() {
+    private void givenTrailDataIsAvailableWithTrailNameForrestPark() {
     }
 
     private void whenSearchTrailWithNameForrestPark() {
+        trail = trailService.fetchByTrailName("Forrest Park");
     }
 
     private void thenReturnTrailID1TrailForForrestPark() {
-        
+        Integer trailID = trail.getTrailID();
+        assertEquals(1, trailID);
     }
 
 }
