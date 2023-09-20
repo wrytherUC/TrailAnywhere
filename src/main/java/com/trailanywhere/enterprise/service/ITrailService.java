2c5051ee909e1eab2e3c2b031582da36f16e714f
wryther <rytherwy@mail.uc.edu>
2023-09-19 22:10:33 -0400

    Created new interface, method

---
+++ src/main/java/com/trailanywhere/enterprise/service/ITrailService.java
@@ -0,0 +1,14 @@
+package com.trailanywhere.enterprise.service;
+
+import com.trailanywhere.enterprise.dto.Trail;
+
+public class ITrailService {
+    /**
+     *
+     * @param trailName - Name of the trail being searched for
+     * @return - return the matching trail
+     */
+    public Trail fetchByTrailName(String trailName) {
+        return null;
+    }
+}
--- src/test/java/com/trailanywhere/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/trailanywhere/enterprise/EnterpriseApplicationTests.java
@@ -1,6 +1,7 @@
 package com.trailanywhere.enterprise;
 
 import com.trailanywhere.enterprise.dto.Trail;
+import com.trailanywhere.enterprise.service.ITrailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
