39876767cd0e03fc256c757d96853eadb0db4436
wryther <rytherwy@mail.uc.edu>
2023-10-01 17:32:20 -0400

    Added trail DAO stub

---
+++ src/main/java/com/trailanywhere/enterprise/dao/TrailDAOStub.java
@@ -0,0 +1,15 @@
+package com.trailanywhere.enterprise.dao;
+
+import com.trailanywhere.enterprise.dto.Trail;
+
+public class TrailDAOStub implements ITrailDAO{
+    @Override
+    public Trail save(Trail trail) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void delete(Trail trail) throws Exception {
+
+    }
+}
