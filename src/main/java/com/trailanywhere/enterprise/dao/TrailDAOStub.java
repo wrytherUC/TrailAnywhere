80497d1fb3fa4912777adf406edf56bb127d0ca7
wryther <rytherwy@mail.uc.edu>
2023-10-01 18:03:35 -0400

    Added spring repo annotation

--- src/main/java/com/trailanywhere/enterprise/dao/TrailDAOStub.java
+++ src/main/java/com/trailanywhere/enterprise/dao/TrailDAOStub.java
@@ -1,10 +1,12 @@
 package com.trailanywhere.enterprise.dao;
 
 import com.trailanywhere.enterprise.dto.Trail;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class TrailDAOStub implements ITrailDAO{
 
     List<Trail> allTrails = new ArrayList<Trail>();
