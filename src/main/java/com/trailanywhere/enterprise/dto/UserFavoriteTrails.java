bc9ec181263f0d7900c51333e4cbb3d68671bfab
Adam Tulloss <artulloss@gmail.com>
2023-10-17 21:17:59 -0400

    @Data annotation should not be used with @Entity - can lead to unexpected behavior

--- src/main/java/com/trailanywhere/enterprise/dto/UserFavoriteTrails.java
+++ src/main/java/com/trailanywhere/enterprise/dto/UserFavoriteTrails.java
@@ -1,10 +1,16 @@
 package com.trailanywhere.enterprise.dto;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 @Entity
-public @Data
+public @Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 class UserFavoriteTrails {
     @EmbeddedId
     UserFavoriteTrailsKey id;
@@ -18,4 +24,17 @@ class UserFavoriteTrails {
     @MapsId("trailID")
     @JoinColumn(name = "trailID")
     Trail trail;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserFavoriteTrails that = (UserFavoriteTrails) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
