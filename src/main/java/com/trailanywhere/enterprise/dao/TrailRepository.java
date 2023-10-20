aff9e6db2cac7de681161fbbec464a0750397a41
Kenny <keotero99@gmail.com>
2023-10-20 11:36:36 -0400

    Fixed entityManager variable name

--- src/main/java/com/trailanywhere/enterprise/dao/TrailRepository.java
+++ src/main/java/com/trailanywhere/enterprise/dao/TrailRepository.java
@@ -90,7 +90,7 @@ public class TrailRepository implements ITrailDAO {
      */
     @Override
     public List<Trail> fetchByDifficulty(String difficulty) {
-        TypedQuery<Trail> query = em.createQuery("SELECT t FROM Trail t WHERE t.difficulty = :DIFFICULTY", Trail.class);
+        TypedQuery<Trail> query = entityManager.createQuery("SELECT t FROM Trail t WHERE t.difficulty = :DIFFICULTY", Trail.class);
         query.setParameter("DIFFICULTY", difficulty);
         return query.getResultList();
     }
@@ -102,7 +102,7 @@ public class TrailRepository implements ITrailDAO {
      */
     @Override
     public List<Trail> fetchByZipCode(String zipCode) {
-        TypedQuery<Trail> query = em.createQuery("SELECT t FROM Trail t WHERE t.zipCode = :ZIP_CODE", Trail.class);
+        TypedQuery<Trail> query = entityManager.createQuery("SELECT t FROM Trail t WHERE t.zipCode = :ZIP_CODE", Trail.class);
         query.setParameter("ZIP_CODE", zipCode);
         return query.getResultList();
     }
