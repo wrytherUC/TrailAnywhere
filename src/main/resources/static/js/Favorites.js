d9030abb8487462cd81e2afe3857e15651a8972e
Kenny <keotero99@gmail.com>
2023-11-08 15:35:44 -0500

    Display favorite trails onto the table

--- src/main/resources/static/js/Favorites.js
+++ src/main/resources/static/js/Favorites.js
@@ -1,6 +1,9 @@
-if (window.location.href.includes("/Favorites")) {
+/**
+ * Fetch trails when the page loads
+ */
+document.addEventListener("DOMContentLoaded", function () {
     getFavoriteTrails();
-}
+});
 
 /**
  * Retrieve favorite trails from backend
@@ -16,17 +19,34 @@ function getFavoriteTrails() {
     xhr.open("POST", "/getFavoriteTrails");
     xhr.onload = function() {
         let response = JSON.parse(xhr.responseText);
-        if (response.length > 0) {
-            console.log("Trail: " + response[0].name);
-            showFavoriteTrails(response);
-        }
+        showFavoriteTrails(response);
     }
     xhr.send(data);
 }
 
 /**
- * Display a user's favorite trails
+ * Display a user's favorite trails onto the table
  */
 function showFavoriteTrails(response) {
-
+    const favoritesTable = document.getElementById("favoritesTable");
+    if (response.length > 0) {
+        for (let i = 0; i < response.length; i++) {
+            let row = favoritesTable.insertRow();
+            let name = row.insertCell(0);
+            let type = row.insertCell(1);
+            let difficulty = row.insertCell(2);
+            let address = row.insertCell(3);
+            let zipCode = row.insertCell(4);
+            name.innerText = response[i].name;
+            type.innerText = response[i].trailType;
+            difficulty.innerText = response[i].difficulty;
+            address.innerText = response[i].address;
+            zipCode.innerText = response[i].zipCode;
+        }
+    } else {
+        let row = favoritesTable.insertRow();
+        let noTrails = row.insertCell(0);
+        noTrails.colSpan = "5";
+        noTrails.innerText = "You have no favorite trails.";
+    }
 }
