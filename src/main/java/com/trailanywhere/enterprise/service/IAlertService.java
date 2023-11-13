222cc2b4b2ec0c0e7dd556a6f74dd87e89ce8c31
wryther <rytherwy@mail.uc.edu>
2023-11-12 22:04:25 -0500

    Removed uneeded import

--- src/main/java/com/trailanywhere/enterprise/service/IAlertService.java
+++ src/main/java/com/trailanywhere/enterprise/service/IAlertService.java
@@ -1,8 +1,6 @@
 package com.trailanywhere.enterprise.service;
 
 import com.trailanywhere.enterprise.dto.Alert;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public interface IAlertService {
