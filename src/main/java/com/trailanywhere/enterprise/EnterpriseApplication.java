c561e4211627d1d006bb4e138fba8e1c3523186b
wryther <rytherwy@mail.uc.edu>
2023-12-06 00:35:36 -0500

    Added missing jdoc

--- src/main/java/com/trailanywhere/enterprise/EnterpriseApplication.java
+++ src/main/java/com/trailanywhere/enterprise/EnterpriseApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * Class initializes the Springboot application, TrailAnywhere
+ */
 @SpringBootApplication
 @EnableCaching
 public class EnterpriseApplication {
