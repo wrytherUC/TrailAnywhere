ea569f2b860cc79b9a85e275b76eb5da1cb0700a
wryther <rytherwy@mail.uc.edu>
2023-12-06 00:34:31 -0500

    Added missing jdoc

--- src/main/java/com/trailanywhere/enterprise/ServletInitializer.java
+++ src/main/java/com/trailanywhere/enterprise/ServletInitializer.java
@@ -3,6 +3,11 @@ package com.trailanywhere.enterprise;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Class required to deploy to Azure App Service
+ * Runs a run a SpringApplication from a traditional WAR deployment.
+ * (<a href="https://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.html">...</a>)
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
